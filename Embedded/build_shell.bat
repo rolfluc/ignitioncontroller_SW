f0ab46ac4e3bed9bd1550f4052f14ff11d79ad2b
Rolfes <lucasatarc@gmail.com>
2024-06-07 22:58:09 -0500

    Various scripts add

---
+++ Embedded/build_shell.bat
@@ -0,0 +1,7 @@
+@echo off
+:: create the actual environment
+@CALL .\tool_config\build_shell\environment.bat
+
+:: start the command shell, set the CMD window title to tell the user that this shell is specially 
+:: made for building the Firmware.
+start "Smack Firmware Build Shell" 
